bea98839f0b7e96cfcda45083fec0e621aa478d5
Rock-AngeL <art_68@inbox.ru>
2015-10-22 16:24:47 +0300

    1.0

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by APopov on 22.10.2015.
+ */
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
